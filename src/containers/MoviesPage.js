143fecc06988e9253f21e607ea8fee7210e9e8cb
paolov1928 <ventura.paolo@gmail.com>
2019-02-14 15:44:41 +0000

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,14 +1,13 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
-import MovieShow from './MovieShow';
+import React from "react"
+import { Route } from "react-router-dom"
+import MoviesList from "../components/MoviesList"
+// import MovieShow from "./MovieShow"
 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-  </div>;
-
+  </div>
 )
 
 export default MoviesPage
